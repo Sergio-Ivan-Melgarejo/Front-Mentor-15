e2b3ffbc46e4fe731ce2bb274ca4ddf8165189ac
Sergio <sergio.ivan.melgarejo@gmail.com>
2022-02-10 16:01:52 -0300

    i added nav

---
+++ starter-code/js/main.js
@@ -0,0 +1,23 @@
+
+const nav = document.getElementById("nav");
+const navButton = document.getElementById("navButton");
+const backgroundMovil = document.getElementById("background-movil");
+
+addEventListener("DOMContentLoaded",()=>{ 
+    navButton.addEventListener("click",()=>{
+        const open = nav.classList.toggle("open");
+        open ? 
+            navButton.children[0].src = "../starter-code/assets/shared/icon-close.svg"
+        :   navButton.children[0].src = "../starter-code/assets/shared/icon-hamburger.svg";
+    })
+
+    backgroundMovil.addEventListener("click",()=>{
+        const open = nav.classList.toggle("open");
+        open ? 
+            navButton.children[0].src = "../starter-code/assets/shared/icon-close.svg"
+        :   navButton.children[0].src = "../starter-code/assets/shared/icon-hamburger.svg";
+    })
+
+    console.log(nav,navButton)
+})
+
